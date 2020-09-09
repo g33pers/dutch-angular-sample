877cbfd4e0bbb57756c1c7b1cbf5fe7b9d3f0d53
g33pers <georges_panis@hotmail.com>
2020-09-09 13:18:29 +0100

    test questions

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError , map } from 'rxjs/operators';
 
 import { IWord } from '../../app/shared/interfaces';
 
@@ -16,6 +16,17 @@ export class DataService {
     getWords() : Observable<IWord[]> {
         return this.http.get<IWord[]>(this.baseUrl + 'words.json')
             .pipe(
+                catchError(this.handleError)
+            );
+    }
+
+    getWord( id: number ) : Observable<IWord> {
+        return this.http.get<IWord[]>(this.baseUrl + 'words.json')
+            .pipe(
+                map( words => {
+                    let word = words.filter((word: IWord) => word.id === id);
+                    return (word && word.length) ? word[0] : null;
+                  }),
                 catchError(this.handleError)
             );
     }
